6e07141eab388fefd83e9c498d7b1acc5ab66f2b
liuyong39281 <hspcadmin@WINDOWS-LMCDHD0.hs.handsome.com.cn>
2021-09-23 20:59:08 +0800

    axios配置化实现

---
+++ examples/config/app.ts
@@ -0,0 +1,60 @@
+import axios from '../../src/index'
+import {TransformFunction} from '../../src/types'
+import qs from 'qs'
+
+axios.defaults.headers.common['test'] = 123
+
+// axios({
+//   url: '/config/post',
+//   method: 'post',
+//   data: qs.stringify({
+//     a: 1
+//   }),
+//   headers: {
+//     test: '321'
+//   }
+// }).then((res) => {
+//   console.log(res.data)
+// })
+
+// axios({
+//   transformRequest: [(function(data) {
+//     return qs.stringify(data)
+//     // return data
+//   }), ...(axios.defaults.transformRequest as TransformFunction[])],
+//   transformResponse: [...(axios.defaults.transformResponse as TransformFunction[]), function(data) {
+//     if (typeof data === 'object') {
+//       data.b = 2
+//     }
+//     return data
+//   }],
+//   url: '/config/post',
+//   method: 'post',
+//   data: {
+//     a: 1
+//   }
+// }).then((res) => {
+//   console.log(res.data)
+// })
+
+const instance = axios.create({
+  transformRequest: [(function(data) {
+    return qs.stringify(data)
+  }), ...(axios.defaults.transformRequest as TransformFunction[])],
+  transformResponse: [...(axios.defaults.transformResponse as TransformFunction[]), function(data) {
+    if (typeof data === 'object') {
+      data.b = 2
+    }
+    return data
+  }]
+})
+
+instance({
+  url: '/config/post',
+  method: 'post',
+  data: {
+    a: 2
+  }
+}).then((res) => {
+  console.log(res.data)
+})
